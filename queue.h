efdea5af2f68cec1ec5ea624ce7eca59f9bb5a97
conorpaschenko <33662221+conorpaschenko@users.noreply.github.com>
2020-01-27 12:41:22 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -25,61 +25,32 @@ class Queue: public Object {
 	
 	// checks if queue has 0 elements
 	bool queue_empty() {
-	   if(this->size == 0) {
-		return true;
-	   }
-	   return false;
+	 
 	}
 
 	// checks if queue has reached capacity
 	bool queue_full() {
-	   if(this->size == this->capacity) {
-		return true;
-	   }
-	   return false;
+	   
 	}
 
 	// returns front element of queue
 	Object* frontElement() {
-	   if(this->queue_empty()) {
-		std:: cout << "queue is empty";
-		exit(1);
-	   }
-	   return this->data[0];
+	
 	}
 	
 	// returns queue size
 	size_t getSize() {
-	   return this->size;
+	   
 	}
 	
 	// adds element to back of queue if queue is not full. Increments back. 
 	void queue_push(Object* element) {
-	   if(this->queue_full()) {
-		std:: cout << "queue is full";
-		exit(1);
-	   }
-	   else {
-		this->data[this->back] = element;
-		this->back++;
-		this->size++;
-	   }
+	  
 	}
 	
 	// removes front element of queue. Shifts remaining elements down a spot	
 	void queue_pop() {
-	   if (this->queue_empty()) {
-		std:: cout << "queue is empty";
-		exit(1);
-	   }
-
-           Object *temp[this->capacity];
-	   for(int i = 0; i < this->size - 1; i++) {
-		temp[i] = temp[i+1];
-	   }
-	   this->data = temp;
-	   this->size--;
-	   this->back--;
+	 
 	}
    
 	~Queue() {
