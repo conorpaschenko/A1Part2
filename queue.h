374f80dd6d9566cf31f1aeb61a57f849a8130188
conorpaschenko <33662221+conorpaschenko@users.noreply.github.com>
2020-01-29 16:54:11 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -8,18 +8,12 @@ class Queue: public Object {
    public:
 	
 
-	// constructor queue that takes in a max capacity
 	Queue() {
 	}
 	
 	// checks if queue has 0 elements
 	bool queue_empty() {
 	 
-	}
-
-	// checks if queue has reached capacity
-	bool queue_full() {
-	   
 	}
 
 	// returns front element of queue
@@ -38,7 +32,7 @@ class Queue: public Object {
 	}
 	
 	// removes front element of queue. Shifts remaining elements down a spot	
-	void queue_pop() {
+	Object* queue_pop() {
 	 
 	}
    
